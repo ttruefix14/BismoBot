f5b360b8521175c5ea0cd0d287c1204d773a8ad0
Yaromir <ttruefix14@gmail.com>
2023-04-15 19:04:56 +0300

    april 15

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+    await message.reply("Введи /ip и узнай актуальный ip-адрес сервера")
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Запусти команду /ip и узнать актуальный ip-адрес сервера")
+    await message.reply("Введи /ip и узнай актуальный ip-адрес сервера")
 
 # @dp.message_handler()
 # async def echo_message(msg: types.Message):
